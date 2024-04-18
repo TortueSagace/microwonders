51ab67ec424df8288ba8d78c6b35b9ac71b6f855
loupwolf <148562941+loupwolf@users.noreply.github.com>
2024-04-18 16:56:03 +0200

    Update header.h

--- TP8/header.h
+++ TP8/header.h
@@ -4,6 +4,6 @@
 #include <time.h>
 
 int notsimd(char* filename, int width, int height);
-int issimd(char* filename, int width, int height);
+int issimd(char* filename, int width, int height, int kernelSize);
 
 char* contourExtraction(char* src,int width, int height);
