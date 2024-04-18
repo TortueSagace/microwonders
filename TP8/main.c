33fea8f2361f4d772fe1d0e02ef7f8199e0ab7b8
loupwolf <148562941+loupwolf@users.noreply.github.com>
2024-04-18 16:55:39 +0200

    Update main.c

--- TP8/main.c
+++ TP8/main.c
@@ -3,6 +3,6 @@
 
 int main() {
     notsimd("Quentin_512x512.raw\0", 512, 512);
-    issimd("Quentin_512x512.raw\0", 512, 512);
+    issimd("Quentin_512x512.raw\0", 512, 512, 3); // 3 for the kernel size
     return 0;
 }
